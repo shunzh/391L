bb0f7f0dd14480e5a65ae1d2543af85e34c63807
Shun Zhang <menie482@gmail.com>
2014-01-17 00:40:17 -0600

    imp the rest. knn not resolved

--- HW1/hw1Classify.m
+++ HW1/hw1Classify.m
@@ -1,42 +1,40 @@
 % load from file
-load digits.mat;
-
-N = 1000;
-M = 200;
-K = 1;
-
-% unroll each data, save in matrix X
-A = [];
-for i = 1 : N
-    img = trainImages(:,:,1,i);
-    A = [A, img(:)];
-end
-
-[m, V] = hw1FindEigendigits(A);
-
-% use only first few eigenvecs
-V_ = V(:, 1:M);
-
-% convert training samples into eigenspace
-trainEigens = [];
-for i = 1 : N
-    I = trainImages(:,:,1,i);
-    I = double(I(:)) - m;
-    I = V_' * I;
+function [accuracy] = hw1Classify(N = 1000, M = 200, K = 2)
+    load digits.mat;
     
-    trainEigens = [trainEigens, I];
-end
-
-% test on test set
-testEigens = [];
-for i = 1 : N
-    I = testImages(:,:,1,i);
-    I = double(I(:)) - m;
-    I = V_' * I;
+    % unroll each data, save in matrix X
+    A = [];
+    for i = 1 : N
+        img = trainImages(:,:,1,i);
+        A = [A, img(:)];
+    end
     
-    testEigens = [testEigens, I];
+    [m, V] = hw1FindEigendigits(A);
+    
+    % use only first few eigenvecs
+    V_ = V(:, 1:M);
+    
+    % convert training samples into eigenspace
+    trainEigens = [];
+    for i = 1 : N
+        I = trainImages(:,:,1,i);
+        I = double(I(:)) - m;
+        I = V_' * I;
+        
+        trainEigens = [trainEigens, I];
+    end
+    
+    % test on test set
+    testEigens = [];
+    for i = 1 : N
+        I = testImages(:,:,1,i);
+        I = double(I(:)) - m;
+        I = V_' * I;
+        
+        testEigens = [testEigens, I];
+    end
+    
+    results = knnclassify(testEigens', trainEigens', trainLabels, K);
+
+    accuracy = sum(results == testLabels(1:N)');
 end
-
-knnclassify(testEigens', trainEigens', trainLabels, K);
-
-% utilities
