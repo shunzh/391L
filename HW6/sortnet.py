a678441c951a34f1d2327d633a30a25f6805ff69
Shun Zhang <menie482@gmail.com>
2014-04-23 21:32:31 -0500

    sortnet

--- HW6/sortnet.py
+++ HW6/sortnet.py
@@ -1,35 +1,72 @@
 import util
+import random
 
-def sort(elem, l):
+class SortNet:
 	"""
-		Basic sort function, sort two elements in l.
-		elem gives the indices.
+		Contain multiple sorting elements
 	"""
-	i = elem[0]
-	j = elem[1]
+	CO_RATE = 0.5
+	MU_RATE = 0.001
 
-	assert len(l) > max(i, j)
+	def __init__(self, maxrange, num):
+		self.elems = [SortElem(maxrange) for i in xrange(num)]
+		self.maxrange = maxrange
+		self.length = num
+		
+	def __getitem__(self,index):
+		return self.elems[index]
 
-	if (l[i] > l[j]):
-		l[i], l[j] = l[j], l[i]
+	def __len__(self):
+		return self.length
 
-def evaluate(elems, pool):
-	"""
-		Evaluate the fitness of elems and pool
-	"""
-	for sample in pool:
-		for elem in elems:
-			sort(elem, sample)
-
-
-def fitness(elems, pool):
-	"""
-		Fitness of given elements on the pool of inputs
-	"""
-	def length():
+	def sort(self, l):
 		"""
-			Shorter length has more fitness
+			Sort l using this sortnet
+			l will be sorted (passed with reference)
 		"""
-		return 1 / len(elems)
+		for elem in self.elems:
+			i = elem[0]
+			j = elem[1]
+
+			assert len(l) > max(i, j)
+
+			if (l[i] > l[j]):
+				l[i], l[j] = l[j], l[i]
 	
-	testLog = evaluate(elems, pool)
+	@staticmethod
+	def crossover(net1, net2):
+		"""
+			Exchange CO_RATE of their elements
+		"""
+		# Their lengths should be equal. Just make sure.
+		num = min(len(net1), len(net2))
+		co_num = int(num * SortNet.CO_RATE)
+
+		for _ in xrange(num):
+			idx = random.randInt(0, num)
+
+			# swap them
+			net1[idx], net2[idx] = net2[idx], net1[idx]
+
+	def mutate(self):
+		"""
+			Randomly change one element with prob. of MU_RATE
+		"""
+		for i in range(self.length):
+			for j in range(2):
+				# with this probability
+				if random.random() < SortNet.MU_RATE:
+					# the i-th element
+					# j \in [0, 1], which is the first or second index
+					self[i][j] = random.randInt(0, self.maxrange)
+
+class SortElem:
+	"""
+		One sorting element.
+		Contain i and j, which are the indices that might be swapped.
+	"""
+	def __init__(self, maxrange):
+		self.indices = [random.randint(0, maxrang), random.randint(0, maxrang)]
+
+	def __getitem__(self,index):
+		return self.indices[index]
